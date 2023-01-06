8e9aa87f86c75a255c9793c3a490a6dcf640b2bd
Paul Blundell <pblundell@twitter.com>
2023-01-06 12:47:42 +0000

    Add dependency on AffectedModuleDetector plugin

--- amd-plugin/plugin/src/main/kotlin/com/blundell/amd/BlundellAffectedModulesPlugin.kt
+++ amd-plugin/plugin/src/main/kotlin/com/blundell/amd/BlundellAffectedModulesPlugin.kt
@@ -1,12 +1,20 @@
 package com.blundell.amd
 
+import com.dropbox.affectedmoduledetector.AffectedModuleConfiguration
+import com.dropbox.affectedmoduledetector.AffectedModuleDetectorPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class BlundellAffectedModulesPlugin : Plugin<Project> {
 
     override fun apply(project: Project) = project.run {
-        println("Initial setup does nothing")
+        project.plugins.apply(AffectedModuleDetectorPlugin::class.java)
+        pluginManager.withPlugin("com.dropbox.affectedmoduledetector") {
+            val config = rootProject.extensions.findByType(AffectedModuleConfiguration::class.java)!!
+            config.logFolder = "${project.buildDir}/amd-output"
+            config.logFilename = "output.log"
+            logger.lifecycle("We can now interact with the plugin programmatically (as above).")
+        }
     }
 
 }
